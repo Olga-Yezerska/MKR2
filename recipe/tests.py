5e11804ef863704affac538a2995e761837dd429
Olga Yezerska <olga_ezerska06@ukr.net>
2026-05-20 14:33:01 +0300

    feat: add tests for main view

--- recipe/tests.py
+++ recipe/tests.py
@@ -47,3 +47,41 @@ class RecipeModelTest(TestCase):
         self.assertIsNotNone(self.recipe.updated_at)
 
 
+class MainViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name='Main Dishes')
+        for i in range(15):
+            Recipe.objects.create(
+                title=f'Recipe {i}',
+                description=f'Description {i}',
+                instructions=f'Instructions {i}',
+                ingredients=f'Ingredients {i}',
+                category=self.category
+            )
+
+    def test_main_view_status_code(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_view_uses_correct_template(self):
+        response = self.client.get(reverse('main'))
+        self.assertTemplateUsed(response, 'recipe/main.html')
+
+    def test_main_view_returns_max_10_recipes(self):
+        response = self.client.get(reverse('main'))
+        self.assertLessEqual(len(response.context['recipes']), 10)
+
+    def test_main_view_returns_recipes_in_context(self):
+        response = self.client.get(reverse('main'))
+        self.assertIn('recipes', response.context)
+
+    def test_main_view_random_on_refresh(self):
+        # With 15 recipes, two requests should occasionally differ (not guaranteed, but tests randomness)
+        titles_1 = set(r.title for r in self.client.get(reverse('main')).context['recipes'])
+        titles_2 = set(r.title for r in self.client.get(reverse('main')).context['recipes'])
+        # Both should have 10 recipes
+        self.assertEqual(len(titles_1), 10)
+        self.assertEqual(len(titles_2), 10)
+
+
