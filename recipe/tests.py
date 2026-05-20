b463505b7fde7de7694b57fab95809fe4b7d83f4
Olga Yezerska <olga_ezerska06@ukr.net>
2026-05-20 14:33:31 +0300

    feat: add test for category detail view

--- recipe/tests.py
+++ recipe/tests.py
@@ -85,3 +85,45 @@ class MainViewTest(TestCase):
         self.assertEqual(len(titles_2), 10)
 
 
+class CategoryDetailViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name='Salads')
+        self.recipe1 = Recipe.objects.create(
+            title='Caesar', description='Caesar salad', instructions='Mix',
+            ingredients='Lettuce, croutons', category=self.category
+        )
+        self.recipe2 = Recipe.objects.create(
+            title='Greek', description='Greek salad', instructions='Chop',
+            ingredients='Tomatoes, olives', category=self.category
+        )
+        self.other_category = Category.objects.create(name='Other')
+
+    def test_category_detail_status_code(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_uses_correct_template(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        self.assertTemplateUsed(response, 'recipe/category_detail.html')
+
+    def test_category_detail_returns_category_in_context(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        self.assertEqual(response.context['category'], self.category)
+
+    def test_category_detail_shows_correct_recipes(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.category.pk}))
+        category = response.context['category']
+        recipe_titles = [r.title for r in category]
+        self.assertIn('Caesar', recipe_titles)
+        self.assertIn('Greek', recipe_titles)
+
+    def test_category_detail_404_for_missing_category(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': 9999}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_category_detail_empty_category(self):
+        response = self.client.get(reverse('category_detail', kwargs={'pk': self.other_category.pk}))
+        self.assertEqual(response.status_code, 200)
+        category = response.context['category']
+        self.assertEqual(list(category), [])
